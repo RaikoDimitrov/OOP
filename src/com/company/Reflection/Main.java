0c179ce8cb68066d317385575bccec0d20861726
Raiko Dimitrov <98freddy98@gmail.com>
2025-01-07 15:34:54 +0200

    Added tasks on Reflection.

--- src/com/company/Reflection/Main.java
+++ src/com/company/Reflection/Main.java
@@ -1,15 +1,21 @@
 package com.company.Reflection;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Arrays;
+import java.lang.reflect.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws NoSuchMethodException {
+
         Class<Reflection> refClazz = Reflection.class;
         System.out.println(refClazz);
         Class<? super Reflection> superClazz = Reflection.class.getSuperclass();
         System.out.println(superClazz);
+
+        //modifiers
+        Field[] declaredFields = refClazz.getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            System.out.println(declaredField.getModifiers() + " -> " + Modifier.toString(declaredField.getModifiers()));
+        }
 
         try {
             Arrays.stream(refClazz.getInterfaces()).map(Class::getSimpleName).forEach(System.out::println);
@@ -22,5 +28,19 @@ public class Main {
             ex.printStackTrace();
         }
 
+        //get methods by annotation
+        Method[] declaredMethods = refClazz.getDeclaredMethods();
+        Map<String, List<String>> methodsByAuthor = new HashMap<>();
+
+        for (Method declaredMethod : declaredMethods) {
+            Author annotation = declaredMethod.getAnnotation(Author.class);
+
+            if (annotation != null) {
+                methodsByAuthor.putIfAbsent(annotation.name(), new ArrayList<>());
+                methodsByAuthor.get(annotation.name()).add(declaredMethod.getName() + "()");
+            }
+        }
+
+        methodsByAuthor.forEach((k, v) -> System.out.printf("%s: %s%n", k, v.toString().replaceAll("[\\[\\],]", "")));
     }
 }
