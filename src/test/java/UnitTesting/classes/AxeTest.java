e245bbb844cb9cacfa4cb83f773bc97d7a56cf51
Raiko Dimitrov <98freddy98@gmail.com>
2025-01-16 01:26:12 +0200

    Added tests to Unit testing.

--- src/test/java/UnitTesting/classes/AxeTest.java
+++ src/test/java/UnitTesting/classes/AxeTest.java
@@ -2,19 +2,34 @@ package UnitTesting.classes;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 
 public class AxeTest {
 
+    private Dummy dummy;
+
+    @BeforeEach
+    public void setUp() {
+        this.dummy = new Dummy(10, 10);
+    }
+
     @Test
-    public void testThatWeaponAttackLosesDurability() {
+    public void testThatAxeAttackLosesDurability() {
         //arrange
         Axe axe = new Axe(10, 10);
-        Dummy dummy = new Dummy(10, 10);
         //act
         axe.attack(dummy);
         //assert
         Assert.assertEquals(9, axe.getDurabilityPoints());
+    }
+
+    @Test(expected = IllegalStateException.class) //assert
+    public void testThatBrokenAxeCantAttack() {
+        //arrange
+        Axe brokenAxe = new Axe(10, 0);
+        //act
+        brokenAxe.attack(dummy);
     }
 
 }
