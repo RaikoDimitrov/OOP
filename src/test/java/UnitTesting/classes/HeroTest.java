8d51e1ff7309a4120d47158e2c11bc453eb5762a
Raiko Dimitrov <98freddy98@gmail.com>
2025-01-16 15:56:10 +0200

    Added mockito dependency and some unit testing.

---
+++ src/test/java/UnitTesting/classes/HeroTest.java
@@ -0,0 +1,30 @@
+package UnitTesting.classes;
+
+import UnitTesting.interfaces.Target;
+import UnitTesting.interfaces.Weapon;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HeroTest {
+
+    private Hero hero;
+
+    @BeforeEach
+    void setUp() {
+
+        Weapon mockedAxe = Mockito.mock(Weapon.class);
+        this.hero = new Hero("Ragnarok", mockedAxe);
+    }
+
+    @Test
+    void testThatHeroGetsXPWhenTargetKilled() {
+        Target mockedTarget = Mockito.mock(Target.class);
+        Mockito.when(mockedTarget.isDead()).thenReturn(true);
+        Mockito.when(mockedTarget.giveExperience()).thenReturn(100);
+        this.hero.attack(mockedTarget);
+        assertEquals(100, this.hero.getExperience());
+    }
+}
